d4686875910faca7d8eac832963a21abb249f6b8
Kczmrz <kczmrz@proton.me>
2023-07-31 15:22:23 +0200

    rebuilding config

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { useTheme } from 'next-themes'
+import { useState, useEffect } from 'react'
+import './navbar.scss';
+
+export default function Navbar()
+{   
+    const [mounted, setMounted] = useState<boolean>(false);
+    const { theme, setTheme, resolvedTheme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
+    const SwitchTheme = () => {
+        if(theme == "dark")
+        {
+            setTheme("light")
+        } else {
+            setTheme("dark")
+        }
+    }
+    return(
+        <nav className="navbar">
+            <p className="text">FastTyper</p>
+            <button onClick={SwitchTheme}> Switch Theme</button>
+        </nav>
+    )
+}
