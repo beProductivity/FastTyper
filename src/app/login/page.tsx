ffc7a3d778cad76d8f9e5180d3fc0b5ae0714ae2
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-08-19 18:58:02 +0200

    Add scaffold for login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import EmailAuth from '@/supabase/auth/EmailAuth';
 import SocialAuth from '@/supabase/auth/SocialAuth';
 import { useRouter } from 'next/router';
+import "./login.scss";
+
 export default function Home() {
   const [login, setLogin] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -18,8 +20,37 @@ export default function Home() {
     discordAuthClient.discord();
   }
     return (
-      <main >
-        Strona Logowania! <br/>
+      <main>
+        <div className='shape'>
+          <div className='wrapper'>
+            <div className="content">
+              <div className="content__login">
+                <div className="content__login--icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3 3H21C22.1046 3 23 3.89543 23 5V19C23 20.1046 22.1046 21 21 21H3C1.89543 21 1 20.1046 1 19V5C1 3.89543 1.89543 3 3 3ZM3 9.61811V19H21V9.61853L12 14.1185L3 9.61811ZM3 7.38199L12 11.8825L21 7.38247V5H3V7.38199Z" fill="black"/>
+                  </svg>
+                </div>
+                <div className="content__login--input">
+
+                </div>
+              </div>
+              <div className="content__password">
+                <div className="content__password--icon">
+                </div>
+                <div className="content__password--input">
+                </div>
+                <div className="content__password--locker">
+                </div>
+              </div>
+          </div>
+        </div>   
+        </div>
+        <div className='bg-image'>
+        </div>
+               
+
+        {/* <img src="./chad.jpg" alt="" /> */}
+        {/* Strona Logowania! <br/>
         Login
         <input type={'text'} value={login} onChange={(event)=> setLogin(event.target.value)}/>
         <br/>
@@ -28,7 +59,7 @@ export default function Home() {
         <br/>
         <button onClick={LoginUser}>Login</button> <br/>
 
-        <button onClick={LoginByDiscord}>DISCORD</button>
+        <button onClick={LoginByDiscord}>DISCORD</button> */}
       </main>
     )
   }
